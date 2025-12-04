55811b8cabd6ca5ccd8baa5839d8371f017a7fe1
wklp <aabalnajjar32@gmail.com>
2025-12-04 12:16:59 +0300

    Initial commit: Grade System project

---
+++ 01-Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter marks saperated by space (Then press enter to submit): ");
+        var input = Console.ReadLine();
+
+        // check if the input is null or empty
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("No marks entered.");
+            return;
+        } 
+
+        var marks = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var mark in marks)
+        {
+            if (int.TryParse(mark, out int score))
+            {
+
+                var grade = GetGrade(score);
+                Console.WriteLine($"Mark: {score}, Grade: {grade}");
+            }
+            else
+                Console.WriteLine($"Invalid input: {mark}");
+
+        }
+    }
+
+    static string GetGrade(int score)
+    {
+        return score switch
+        {
+          > 100 or < 0 => "Invalid Score",  
+          >= 90 => "A",
+          >= 80 => "B",
+          >= 70 => "C",  
+          >= 60 => "D",
+          _   => "F"
+        };
+    }
+}
